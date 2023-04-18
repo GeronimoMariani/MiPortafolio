1b01111666c87dacde3a8a2f256cb56fa3c2209e
Geronimo Mariani <geronimomariani5@gmail.com>
2023-04-18 05:31:50 -0300

    se agrego form.php

---
+++ form.php
@@ -0,0 +1,19 @@
+<?php
+
+$nombre = $_POST['nombre']
+$email = $_POST['email']
+$mensaje = $_POST['textarea']
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje = "Su e-mail es " . $email . " \r\n";
+$mensaje = "Mensaje: " . $_POST['textarea'] . " \r\n";
+$mensaje = "Enviado el " . date('d/m/Y', time());
+
+$para = 'geronimomariani5@gmail.com';
+$asunto = 'Este mail fue enviado desde mi portafolio';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header('Location:exito.html');
+
+?>
